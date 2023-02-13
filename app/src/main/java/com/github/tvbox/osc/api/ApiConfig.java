bf58b0267a443c86375747d173973ee53bb18e7a
ants_double <ants_double@yeah.net>
2023-02-13 16:34:16 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://cdn.jsdelivr.net/gh/Ants-double/tvbox_config@1.0/tv.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
